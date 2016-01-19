01cd93950de0abde02ad82e095673384cd69c10d
Jonathan Tobin <jonny.tobin24@gmail.com>
2016-01-19 15:16:39 +0000

    Playing with variables in JS

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 // Just printing to the console
 console.log("Hello World!!");
+
+function add(num01, num02) {
+	return num01+num02;
+}
+
+var theAns = add(2,2);
+console.log("2 + 2 is " +theAns);
+
+var name = "Jonny";
+var age = 21;
+var male = true;
+var myArray = new Array();
+
+if (age == 21) {
+	console.log("You are still young " +name);
+}
+
+var o = new Object();
+
+var anotherObject = {};
