601dd7f0c25e4feef2bf8bb8056e845220d72496
Jonathan Tobin <jonny.tobin24@gmail.com>
2016-01-19 15:38:46 +0000

    Exploring objects in JS

--- app.js
+++ app.js
@@ -20,3 +20,51 @@ if (age == 21) {
 var o = new Object();
 
 var anotherObject = {};
+
+/* 
+AS:
+public class person {
+	public var name:string;
+	public var age:int;
+	public var male:boolen;
+
+	function person() {
+		//default to female
+		this.male = false;
+	}
+
+	public growOld():void {
+		this.age ++;
+	}
+}
+
+//Somewhere else
+var p:Person = new person();
+p.name = jonny;
+p.age = 21;
+p.male = true;
+
+p.growOld();
+*/
+var person = {};
+person.name = "jonny";
+person.age = 21;
+person.male = true;
+person.growOld = function() {
+	this.age++;
+};
+
+var anotherPerson = {
+	name: "Tom",
+	age: 25,
+	male: false,
+	growOld: function() {
+		this.age++;
+	}, 
+	friends: ["Tom", "Pat"]
+};
+
+for (var i=0; i<10; i++) {
+	console.log(anotherPerson.name + " is " + anotherPerson.age);
+	anotherPerson.growOld();
+}
